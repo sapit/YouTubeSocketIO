b7082bdae3b598f01a64844f0e1cce544f364cb6
Zdravko Ivanov <zdravkozzz13@gmail.com>
2017-03-18 18:07:58 +0000

    Mostly working, first computer is a master. Name of song is shown. Like/Dislike did not work - probably because of line 'queue=q' on initialise

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var master = -1
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  if(master==-1)
+  {
+    master=1;
+    socket.emit("master");
+  }
+  else{
+    io.emit("getQueue")
+  }
+  
+  socket.on("giveQueue", function(q, ql, id){
+    console.log("initialise");
+    console.log(q);
+    console.log(ql);
+    io.emit("initialise", {q, ql, id});
+  });
+
+
   socket.on('chat message', function(msg){
-    console.log(msg);
     io.emit('chat message', msg);
   });
   socket.on('songChange', function(id){
-      console.log(id);
       io.emit("songChanged", id);
   });
+
+  socket.on('vote', function(id, vote){
+      io.emit("vote", id, vote);
+  });
+
+  socket.on('stop', function(){
+      io.emit("stop");
+  });
+
+  socket.on('play', function(){
+      io.emit("play");
+  });
+
+  socket.on('next', function(){
+      io.emit("next");
+  });
+
+  socket.on('deQueue', function(id){
+      console.log(id);
+      socket.broadcast.emit("deQueue", id);
+  });
+
+  socket.on('suggestById', function(){
+      socket.broadcast.emit("getId");
+  });
+
+  socket.on("getId",function(id){
+      socket.broadcast.emit("suggestById",id);
+  });
+
+  socket.on("updateQueue",function(q){});
 
 });
 
